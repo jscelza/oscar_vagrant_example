3aabaa43fd79421d440365b08b7697bde94e53cc
Jeff Scelza <jeffscelza76@gmail.com>
2015-03-12 02:22:14 -0600

    More clean up and getting ready for demo

--- puppet/module_magic.sh
+++ puppet/module_magic.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
 ACTION=$1
-R10K_ENVIRONMENT="production"
+
+if [ $# == 2 ]; then
+  R10K_ENVIRONMENT="$2"
+else
+  echo "No environment set. Defaulting to 'production'."
+  R10K_ENVIRONMENT="production"
+fi
 
 BASE_DIR="${HOME}/pe_r10k_files"
 R10K_CACHE_DIR="${BASE_DIR}/cache/r10k"
 R10K_HIERA_DIR="${BASE_DIR}/hiera"
 R10K_PUPPETFILE_DIR="${BASE_DIR}/environments"
-R10K_YAML_FILE="${BASE_DIR}/r10k.yaml"
+R10K_YAML_FILE="./r10k.yaml"
+R10K_REMOTE_PUPPETFILE="https://github.webapps.rr.com/fylgia/puppetfile.git"
+REMOTE_HIERA="git@github.webapps.rr.com:fylgia/hieradata.git"
 
 PE_MODULE_DIR="${HOME}/pe_modules"
 PE_HIERA_DIR="${HOME}/pe_hiera"
@@ -16,34 +24,42 @@ PE_HIERA_DIR="${HOME}/pe_hiera"
 #
 # Check and Create directories
 checkAndCreateDir () {
+  echo "checking to ensure r10k is installed"
+  GEM_OUTPUT=`gem list | grep r10k`
+  if [ "${GEM_OUTPUT}" == "" ]; then
+    gem install r10k || { echo 'Installation of r10k failed' ; exit 1; }
+  fi
+
   echo "Verifying directories for use with r10k"
-  if [ ! -d "${R10K_PUPPETFILE_DIR}" ]; then
-    echo "Creating ${R10K_PUPPETFILE_DIR}"
-    mkdir -p ${R10K_PUPPETFILE_DIR}
+  if [ ! -d "${R10K_PUPPETFILE_DIR}/${R10K_ENVIRONMENT}" ]; then
+    echo "Creating ${R10K_PUPPETFILE_DIR}/${R10K_ENVIRONMENT}"
+    mkdir -p ${R10K_PUPPETFILE_DIR}/${R10K_ENVIRONMENT}
   fi
-  if [ ! -d "${R10K_HIERA_DIR}" ]; then
-    echo "Creating ${R10K_HIERA_DIR}"
-    mkdir -p ${R10K_HIERA_DIR}
-  fi
+
+  # if [ ! -d "${R10K_HIERA_DIR}/${R10K_ENVIRONMENT}" ]; then
+  #   echo "Creating ${R10K_HIERA_DIR}/${R10K_ENVIRONMENT}"
+  #   mkdir -p ${R10K_HIERA_DIR}/${R10K_ENVIRONMENT}
+  # fi
+
+  mkdir -p ${R10K_CACHE_DIR}
+  chmod 777 ${R10K_CACHE_DIR}
 }
 ############
 #
 # Create r10k.yaml file
 createR10kFile () {
 
-  echo "Creating r10k yaml file"
+  echo "Creating ${R10K_YAML_FILE} file"
   cat /dev/null > ${R10K_YAML_FILE}
 
-  cat << EOT >> ${R10K_YAML_FILE}
-  :cachedir: "${R10K_CACHE_DIR}"
-  :sources:
-    :operations:
-      remote: 'git@github.webapps.rr.com:fylgia/puppetfile.git'
-      basedir: "${R10K_PUPPETFILE_DIR}"
-    :hiera:
-      remote: 'git@github.webapps.rr.com:fylgia/hieradata.git'
-      basedir: "${R10K_HIERA_DIR}"
-  EOT
+#To avoid syntax problem this sections need to be aligned to first column
+cat << EOT >> ${R10K_YAML_FILE}
+:cachedir: "${R10K_CACHE_DIR}"
+:sources:
+  :local:
+    remote: "${R10K_REMOTE_PUPPETFILE}"
+    basedir: "${R10K_PUPPETFILE_DIR}"
+EOT
 
 }
 ############
@@ -59,9 +75,7 @@ case ${ACTION} in
     checkAndCreateDir
     createR10kFile
 
-    mkdir -p ${R10K_CACHE_DIR}
-    chmod 777 ${R10K_CACHE_DIR}
-    r10k deploy -v -c ${R10K_YAML_FILE} environment ${R10K_ENVIRONMENT} -p
+    r10k deploy -v debug environment ${R10K_ENVIRONMENT} -p
 
     WORKING_MODULES_DIR="${R10K_PUPPETFILE_DIR}/${R10K_ENVIRONMENT}/modules"
     WORKING_MODULE_LIST=`ls -1 ${WORKING_MODULES_DIR}`
@@ -78,17 +92,16 @@ case ${ACTION} in
       fi
     done
 
-    echo "\tRemoving remote entry for cache on hiera"
-    cd ${R10K_HIERA_DIR}/${R10K_ENVIRONMENT}
-    git remote rm cache
-    git checkout master
+    echo "\tObtaining Hiera repository"
+    cd ${HOME}
+    git clone ${REMOTE_HIERA} --branch ${R10K_ENVIRONMENT} pe_hiera
+
     mv ${WORKING_MODULES_DIR} ${PE_MODULE_DIR}
-    mv ${R10K_HIERA_DIR}/${R10K_ENVIRONMENT} ${PE_HIERA_DIR}
-    MESSAGE="Environment ${R10K_ENVIRONMENT} as been set up at ~/pe_modules and ~/pe_hiera. \n Do not forget to change your branch"
+    MESSAGE="\n\nEnvironment ${R10K_ENVIRONMENT} as been set up at ~/pe_modules and ~/pe_hiera. \n Do not forget to create a new branch for your module"
 
     ;;
   cleanup)
-    DELETE_LIST="${PE_HIERA_DIR} ${PE_MODULE_DIR} ${R10K_CACHE_DIR} ${R10K_HIERA_DIR} ${R10K_PUPPETFILE_DIR} ${R10K_YAML_FILE}"
+    DELETE_LIST="${PE_HIERA_DIR} ${PE_MODULE_DIR} ${R10K_CACHE_DIR} ${R10K_PUPPETFILE_DIR} ${R10K_YAML_FILE}"
     echo "Removing: ${DELETE_LIST}"
     rm -rf ${DELETE_LIST}
     MESSAGE="Removal of files compelete"
